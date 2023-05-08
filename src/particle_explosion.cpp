c4831e135a1614e59a0d7f66022b9b5f91b2ff6e
Louise Cuthbertson <lcuthbertson_3112@hotmail.co.uk>
2023-05-08 21:55:39 +0100

    colour animation added

--- src/particle_explosion.cpp
+++ src/particle_explosion.cpp
@@ -9,6 +9,7 @@
 // Using SDL 2 library as a GUI API. SDL2 was installed in terminal by typing "brew install sdl2"
 
 #include <iostream>
+#include <math.h>
 #include "Screen.h"
 
 using namespace std;
@@ -22,14 +23,25 @@ int main() {
 		cout << "Error initialising SDL." << endl;
 	}
 
+	int max = 0;
+
 	while (true) {
 		// Update particles
 
 
 		// Draw particles:
+		int msElapsed = SDL_GetTicks();
+		unsigned char red = (1 + sin(msElapsed * 0.001)) * 128;
+		unsigned char green = (1 + sin(msElapsed * 0.002)) * 128;
+		unsigned char blue = (1 + sin(msElapsed * 0.003)) * 128;
+
+		if (green > max) {
+			max = green;
+		}
+
 		for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
 			for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
-				screen.setPixel(x, y, 0, 255, 0);
+				screen.setPixel(x, y, red, green, blue);
 			}
 		}
 
